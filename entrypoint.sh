92c3695c9f70526d8ba06aeaf74f96682f065656
Kyle Harding <kmharding@pm.me>
2020-07-04 18:23:50 -0400

    use custom config file as per documentation

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-ZMCONF=/etc/zm/zm.conf
+ZMCONF=/etc/zm/conf.d/03-custom.conf
 EVENTSINI=/etc/zm/zmeventnotification.ini
 SECRETSINI=/etc/zm/secrets.ini
-PHPINI=/etc/php/7.2/apache2/php.ini
-
-TZ="${TZ:-"UTC"}"
-SHMEM="${SHMEM:-"50%"}"
-ZM_DB_HOST="${ZM_DB_HOST:-"mariadb"}"
-ZM_DB_USER="${ZM_DB_USER:-"zmuser"}"
-ZM_DB_PASS="${ZM_DB_PASS:-"zmpass"}"
-ZM_DB_NAME="${ZM_DB_NAME:-"zm"}"
 
 cleanup () {
     /usr/sbin/apache2 -k stop
@@ -20,21 +12,14 @@ cleanup () {
 
 trap cleanup SIGTERM
 
-umount -v /dev/shm
-mount -v -t tmpfs -o rw,nosuid,nodev,noexec,relatime,size="${SHMEM}" tmpfs /dev/shm
-
-for uuid in $(blkid -sUUID -ovalue /dev/sd??)
-do
-    mkdir -v /media/"${uuid}" 2>/dev/null
-    mount -v UUID="${uuid}" /media/"${uuid}"
-    chown -v www-data:www-data /media/"${uuid}"
-done
-
+# https://zoneminder.readthedocs.io/en/stable/userguide/configfiles.html
 # https://github.com/ZoneMinder/zoneminder/blob/master/zm.conf.in
-sed "s|ZM_DB_NAME=.*$|ZM_DB_NAME=${ZM_DB_NAME}|" -i "${ZMCONF}"
-sed "s|ZM_DB_USER=.*$|ZM_DB_USER=${ZM_DB_USER}|" -i "${ZMCONF}"
-sed "s|ZM_DB_PASS=.*$|ZM_DB_PASS=${ZM_DB_PASS}|" -i "${ZMCONF}"
-sed "s|ZM_DB_HOST=.*$|ZM_DB_HOST=${ZM_DB_HOST}|" -i "${ZMCONF}"
+cat > "${ZMCONF}" << EOF
+ZM_DB_HOST=${ZM_DB_HOST:-mariadb}
+ZM_DB_USER=${ZM_DB_USER:-zmuser}
+ZM_DB_PASS=${ZM_DB_PASS:-zmpass}
+ZM_DB_NAME=${ZM_DB_NAME:-zm}
+EOF
 
 # https://github.com/pliablepixels/zmeventnotification/blob/master/secrets.ini
 [ -n "${ZM_USER}" ]                         && crudini --verbose --set --inplace "${SECRETSINI}" secrets ZM_USER "${ZM_USER}"
@@ -65,9 +50,15 @@ crudini --verbose --set --inplace "${EVENTSINI}" ssl enable "${SSL_ENABLE:-no}"
 crudini --verbose --set --inplace "${EVENTSINI}" ssl cert "!ES_CERT_FILE"
 crudini --verbose --set --inplace "${EVENTSINI}" ssl key "!ES_KEY_FILE"
 
-echo "date.timezone = ${TZ}" >> "${PHPINI}"
-ln -sf "/usr/share/zoneinfo/${TZ}" /etc/localtime
-echo "${TZ}" > /etc/timezone
+umount -v /dev/shm
+mount -v -t tmpfs -o rw,nosuid,nodev,noexec,relatime,size="${SHMEM:-"50%"}" tmpfs /dev/shm
+
+for uuid in $(blkid -sUUID -ovalue /dev/sd??)
+do
+    mkdir -v /media/"${uuid}" 2>/dev/null
+    mount -v UUID="${uuid}" /media/"${uuid}"
+    chown -v www-data:www-data /media/"${uuid}"
+done
 
 if ! /usr/bin/mysqlshow -u"${ZM_DB_USER}" -p"${ZM_DB_PASS}" -h"${ZM_DB_HOST}" "${ZM_DB_NAME}" 1>/dev/null
 then
