1d78c3f4b11fbad242da38adeb5f417dea01051a
Kyle Harding <kmharding@pm.me>
2020-12-27 22:43:06 -0500

    db: dump all databases
    Rather than specifying the specific app database, just have mysqldump
    export all databases.
    
    Change-type: patch
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- db/sqldump.sh
+++ db/sqldump.sh
@@ -6,8 +6,6 @@ while :
 do
     sleep 1h
     mysqldump --single-transaction \
-        -h db \
-        -u root \
-        -p"${MYSQL_ROOT_PASSWORD}" \
-        "${MYSQL_DATABASE}" > "/var/lib/mysql/${MYSQL_DATABASE}.sqldump"
+        -h db -u root -p"${MYSQL_ROOT_PASSWORD}" \
+        --all-databases > "/var/lib/mysql/backup.sql"
 done
